ffc963e337484b1f4ae8545384ac35d498b35c8c
Yodai-Toda <2001daiyousama@gmail.com>
2020-08-27 10:08:16 +0900

    自機の実装

--- DirectX2DLibraryCpp/Src/Main.cpp
+++ DirectX2DLibraryCpp/Src/Main.cpp
@@ -5,10 +5,15 @@
 #include "Engine/Engine.h"
 #include "Common/Vec.h"
 
+float g_Player_x = 50.0f;		// プレイヤーのx座標
+float g_Player_y = 215.0f;		// プレイヤーのy座標
+
 // ゲーム処理
 void GameProcessing();
 // 描画処理
 void DrawProcessing();
+
+void PlayerMove();
 
 /*
 	エントリポイント
@@ -25,6 +30,9 @@ int WINAPI WinMain(
 	{
 		return 0;
 	}
+
+	// 画像読み込み
+	Engine::LoadTexture("Player", "Res/Player.png");
 
 	while (true)
 	{
@@ -70,6 +78,8 @@ void GameProcessing()
 	// 入力データの更新
 	Engine::Update();
 
+	PlayerMove();
+
 }
 
 void DrawProcessing()
@@ -78,9 +88,31 @@ void DrawProcessing()
 	// 描画処理を実行する場合、必ず最初実行する
 	Engine::StartDrawing(0);
 
-
+	// テクスチャ描画
+	Engine::DrawTexture(g_Player_x, g_Player_y, "Player");
 
 	// 描画終了
 	// 描画処理を終了する場合、必ず最後に実行する
 	Engine::FinishDrawing();
+}
+
+// プレイヤーの移動
+void PlayerMove()
+{
+	if (Engine::IsKeyboardKeyHeld(DIK_UP)&& g_Player_y >= 0)
+	{	
+		g_Player_y -= 2.0f;		
+	}
+	if (Engine::IsKeyboardKeyHeld(DIK_DOWN)&& g_Player_y <= 420)
+	{
+		g_Player_y += 2.0f;
+	}
+	if (Engine::IsKeyboardKeyHeld(DIK_LEFT)&& g_Player_x >= 0)
+	{
+		g_Player_x -= 2.0f;	
+	}
+	if (Engine::IsKeyboardKeyHeld(DIK_RIGHT)&& g_Player_x <= 580)
+	{	
+		g_Player_x += 2.0f;	
+	}
 }
